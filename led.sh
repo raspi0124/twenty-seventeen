9b2bdcb2b39238b76485f49d91038cd8b4b92066
raspi0124 <adores0124@yahoo.co.jp>
2016-11-16 09:38:14 +0100

    Create led.sh
    The sh file for start programs

---
+++ led.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PYTHON-SERVICE1='LED1.py'
+SCNAME1='led1'
+
+echo starting_all_programs
+screen -AmdS $SCNAME1 python $PYTHON-SERVICE1
+echo finished-1
+sleep 5
+echo starting led2.py
+sleep 5
+screen -AmdS $SCNAME2 python $PYTHON-SERVICE2
+echo finished-2
+sleep 5
+screen -AmdS $SCNAME3 ruby $RUBY-SERVICE3
+sleep 5
+echo the_all_programs_was_started
