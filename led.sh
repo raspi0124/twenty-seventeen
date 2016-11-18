8049fcd30cfb594f9c756afa13d1e6a5515b0caa
raspi0124 <adores0124@yahoo.co.jp>
2016-11-18 15:19:24 +0100

    Changed to background task

--- led.sh
+++ led.sh
@@ -3,15 +3,4 @@
 PYTHON-SERVICE1='LED1.py'
 SCNAME1='led1'
 
-echo starting_all_programs
-screen -AmdS $SCNAME1 python $PYTHON-SERVICE1
-echo finished-1
-sleep 5
-echo starting led2.py
-sleep 5
-screen -AmdS $SCNAME2 python $PYTHON-SERVICE2
-echo finished-2
-sleep 5
-screen -AmdS $SCNAME3 ruby $RUBY-SERVICE3
-sleep 5
-echo the_all_programs_was_started
+echo starting_all_programs　& screen -AmdS $SCNAME1 python $PYTHON-SERVICE1 & echo finished-1 & sleep 5 & echo starting led2.py & sleep 5 & screen -AmdS $SCNAME2 python $PYTHON-SERVICE2 & echo finished-2 & sleep 5 & screen -AmdS $SCNAME3 ruby $RUBY-SERVICE3 & sleep 5 & echo the_all_programs_was_started
